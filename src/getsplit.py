dfe9f86f516036e4618584180506ce2eb01d6426
JohnMWall <43920776+JohnMWall@users.noreply.github.com>
2020-05-24 02:26:20 -0500

    50 english

--- src/getsplit.py
+++ src/getsplit.py
@@ -2,32 +2,6 @@ import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
 
-
-
-
-# def filter_df(df):
-#     '''
-#     Function to filter audio files based on df columns
-#     df column options: [age,age_of_english_onset,age_sex,birth_place,english_learning_method,
-#     english_residence,length_of_english_residence,native_language,other_languages,sex]
-#     :param df (DataFrame): Full unfiltered DataFrame
-#     :return (DataFrame): Filtered DataFrame
-#     '''
-#
-#     # Example to filter arabic, mandarin, and english and limit to 73 audio files
-#     arabic = df[df['native_language'] == 'arabic']
-#     mandarin = df[df['native_language'] == 'mandarin']
-#     english = df[df.native_language == 'english'][:73]
-#     mandarin = mandarin[mandarin.length_of_english_residence < 10][:73]
-#     arabic = arabic[arabic.length_of_english_residence < 10][:73]
-#
-#     df = english.append(arabic)
-#     df = df.append(mandarin)
-#
-#
-#
-#     return df
-
 def filter_df(df):
     '''
     Function to filter audio files based on df columns
@@ -36,7 +10,7 @@ def filter_df(df):
     :param df (DataFrame): Full unfiltered DataFrame
     :return (DataFrame): Filtered DataFrame
     '''
-
+"""
     arabic = df[df.native_language_rev.str.contains('arabic')]
     mandarin = df[df.native_language_rev.str.contains('mandarin')]
     english = df[df.native_language_rev.str.contains('english')][:125]
@@ -47,17 +21,16 @@ def filter_df(df):
     print(english.shape)
     print(mandarin.shape)
     print(arabic.shape)
-
+"""
     # df = df.append(english)
     # df = df.append(arabic)
     # df = df.append(mandarin)
 
-    df_new = english.append(mandarin).append(arabic)
     # print(df_new.info(verbose=True))
 
     # df_new['birth_place'].apply(lambda col: col['birth_place'] = col['birth_place'].str[-1])
 
-    return df
+    return df[df.native_language_rev.str.contains('english')][:50]
 
 def split_people(df,test_size=0.2):
     '''
